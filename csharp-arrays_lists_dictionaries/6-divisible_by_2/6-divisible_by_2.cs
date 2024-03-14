086cdf8258c43d099ec8a0a50be19fb3573889de
Mxn-ptr <maxence0@hotmail.fr>
2024-03-14 15:07:12 +0100

    Add task 6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> div = new List<bool>();
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+                div.Add(true);
+            else
+                div.Add(false);
+        }
+        return div;
+    }
+}
