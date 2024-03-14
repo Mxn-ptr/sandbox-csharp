e753d5919d3a95a694f2c0b48745bc1ca25e1c3d
Mxn-ptr <maxence0@hotmail.fr>
2024-03-14 14:42:36 +0100

    Add task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < size; i++)
+            list.Add(i);
+        foreach (int integer in list)
+        {
+            Console.Write(integer);
+            if (integer != list.Capacity - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return list;
+    }
+}
