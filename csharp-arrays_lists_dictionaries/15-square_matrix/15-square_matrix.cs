60eaf0a2a5943794061aea3b8b961c1dec42f1f8
Mxn-ptr <maxence0@hotmail.fr>
2024-04-02 10:47:42 +0200

    Add task 15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] copiedArray = (int[,])myMatrix.Clone();
+
+        for (int i = 0; i < copiedArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < copiedArray.GetLength(1); j++)
+            {
+                copiedArray[i, j] *= copiedArray[i, j];
+            }
+        }
+        return copiedArray;
+    }
+}
