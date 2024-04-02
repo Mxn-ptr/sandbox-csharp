8f2500de3274ac9debe39e0ef5b1f187e80a1027
Mxn-ptr <maxence0@hotmail.fr>
2024-04-02 10:27:35 +0200

    Add task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = 0;
+        string key = "None";
+        foreach (var kvp in myList)
+        {
+            if (kvp.Value > max) {
+                max = kvp.Value;
+                key = kvp.Key;
+            }
+        }
+        return key;
+    }
+}
